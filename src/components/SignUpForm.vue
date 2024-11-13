509c854f056890498bfbb98d5c3281f1c2e84518
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-11-13 17:27:01 +0100

    click event

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -15,7 +15,9 @@
     <label>Skills:</label>
     <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="removeSkill(skill)">
+        {{ skill }}
+      </span>
     </div>
 
     <div class="terms">
@@ -50,6 +52,11 @@ export default {
 
         this.tempSkill = "";
       }
+    },
+    removeSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return item !== skill;
+      });
     },
   },
 };
@@ -89,5 +96,17 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
