6e6a059b1b077f18ef28735dfe4a1302cc0a81ce
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-11-13 18:17:31 +0100

    forms and inputs

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email</label>
     <input type="emai" required v-model="email" />
 
     <label>Password</label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -24,6 +25,10 @@
       <input type="checkbox" required v-model="terms" />
       <label>Accept terms and conditions</label>
     </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
@@ -41,6 +46,7 @@ export default {
       terms: false,
       tempSkill: "",
       skills: [],
+      passwordError: "",
     };
   },
   methods: {
@@ -57,6 +63,20 @@ export default {
       this.skills = this.skills.filter((item) => {
         return item !== skill;
       });
+    },
+    handleSubmit() {
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 chars long";
+
+      if (!this.passwordError) {
+        console.log("email", this.email);
+        console.log("password", this.password);
+        console.log("role", this.role);
+        console.log("skills", this.skills);
+        console.log("terms accepted", this.terms);
+      }
     },
   },
 };
@@ -108,5 +128,22 @@ input[type="checkbox"] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
