0e2fbc27c914ccffd6ad85c335a6e79d2ed10cc0
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-11-13 13:53:33 +0100

    checkboxes

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -11,10 +11,16 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+      <input type="checkbox" required v-model="terms" />
+      <label>Accept terms and conditions</label>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
+  <p>Terms accepted: {{ terms }}</p>
 </template>
 
 <script>
@@ -24,6 +30,7 @@ export default {
       email: "",
       password: "",
       role: "",
+      terms: false,
     };
   },
 };
@@ -56,5 +63,12 @@ select {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
